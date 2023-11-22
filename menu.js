621596b9d0d1d271c9830ff50e2560c6ae7719d3
joakonicolaa <151783505+joakonicolaa@users.noreply.github.com>
2023-11-22 16:56:38 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,5 @@
+const fabars=document.querySelector(".fa-bars");
+const navmenu=document.querySelector(".nav-menu");
+fabars.addEventListener("click",()=>{
+    navmenu.classList.toggle("clasemenu");
+} );
